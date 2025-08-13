8f12055991b784809781fd665d143d4ba6c477e9
lanbaoshen <lanbaoshen@icloud.com>
2025-08-13 17:05:48 +0800

    Refactor imports in __init__.py and update ignored files in pyproject.toml

--- components/user/__init__.py
+++ components/user/__init__.py
@@ -1,10 +1,10 @@
 from .email_input import EmailInput
-from .forgot_password import ForgotPassword
 from .my_profile import MyProfile
 from .name_input import NameInput
 from .password_input import PasswordInput
 from .update_password import UpdatePassword
 from .username_input import UsernameInput
+from .forgot_password import ForgotPassword
 
 __all__ = [
     'EmailInput',
